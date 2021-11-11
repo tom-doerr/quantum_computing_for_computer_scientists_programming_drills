d918dbd02828716987d569db977afb61d18f7133
Tom Dörr <tom.doerr@tum.de>
2021-11-11 17:48:31 +0100

    Add file

---
+++ e2_1_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def addition(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+def inverse(a):
+    return (-a[0], -a[1])
+
+def scalar_multiplication(s, a):
+    return (s * a[0], s * a[1])
+
